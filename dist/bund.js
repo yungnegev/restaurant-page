7d34169b5a55ad77a081056850bdc101a87f50dc
Yung <neiklotrr@gmail.com>
2022-11-12 14:48:13 +0300

    Boilerplate and webpack setup

---
+++ dist/bund.js
@@ -0,0 +1,10 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+
+console.log('wagwan')
+/******/ })()
+;
+//# sourceMappingURL=bund.js.map
---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports ={
+    mode: 'development',
+    entry: path.resolve(__dirname, 'src/index.js'),
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bund.js',
+        clean: true,
+        assetModuleFilename: '[name][ext]'
+    },
+    devtool: 'source-map',
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Restaurant',
+            filename: 'index.html',
+            template: 'src/template.html',
+        })
+    ]
+}
